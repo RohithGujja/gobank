aac4c31a5844f72ee9275c347abac4f175d57f68
Rohith Gujja <rohith.gujja@netcracker.com>
2023-08-15 18:10:18 +0530

    implement server - initials

---
+++ api.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandlerFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
+
+type APIServer struct {
+	listenAddr string
+}
+
+func NewAPIServer(addr string) *APIServer {
+	return &APIServer{
+		listenAddr: addr,
+	}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHTTPHandlerFunc(s.handleAccount))
+
+	router.HandleFunc("/account/{id}", makeHTTPHandlerFunc(s.handleGetAccount))
+
+	log.Println("API server is running on port:", s.listenAddr)
+
+	err := http.ListenAndServe(s.listenAddr, router)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	switch r.Method {
+	case http.MethodGet:
+		return s.handleGetAccount(w, r)
+	case http.MethodPost:
+		return s.handleCreateAccount(w, r)
+	case http.MethodDelete:
+		return s.handleDeleteAccount(w, r)
+	default:
+		return fmt.Errorf("method not allowed, %s", r.Method)
+	}
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	id := mux.Vars(r)["id"]
+	fmt.Println("id:", id)
+
+	return WriteJSON(w, http.StatusOK, &Account{})
+}
+
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	//account := NewAccount("Rohith", "G")
+	return nil
+}
+
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
